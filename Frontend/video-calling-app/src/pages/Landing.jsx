559c1b1d100ee18174846de2a6b49a5e53548d72
Aakriti <bomzanaakriti@gmail.com>
2026-03-25 22:15:02 +0545

    Added CSS to landing page and created ui for the landingpage

--- Frontend/video-calling-app/src/pages/Landing.jsx
+++ Frontend/video-calling-app/src/pages/Landing.jsx
@@ -6,7 +6,25 @@ export default function Landing(){
     return(
         <div className="landingPageContainer"
         >
-            Landing Page
+            <nav>
+                <div className="navHeader">
+                    <h2>Virtual,but real.</h2>
+                </div>
+                <div className="nav-list">
+                    <p> Join as guest</p>
+                    <p>Register</p>
+                    <div role="button">
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+           
+<div className="landingMainContainer">
+    <div>
+        <h1> <span style={{color:"#FFA500"}}>Connect </span>with more people.</h1>
+    </div>
+</div>
+
         </div>
     )
 }
