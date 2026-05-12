e206917546a1db222b1f47fea2f2c9c73875f8c1
Aakriti <bomzanaakriti@gmail.com>
2026-05-12 20:57:29 +0545

    Added videoMeet component

---
+++ Frontend/video-calling-app/src/pages/videoMeet.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const server_url = "http://localhost:5173/";
+
+var connections ={};
+
+const peerConfigConnections ={
+    "iceServers":[
+        {"urls": "stun:stun.l.google.com:19302"}
+    ]
+} 
+
+
+export default function videoMeet(){
+
+
+
+    return(
+        <div>VideoMeet</div>
+    )
+} 
