5fb9106cfc0a71b8d11cf4ae935f65c255ee72cb
Aakriti <bomzanaakriti@gmail.com>
2026-05-21 22:11:34 +0545

    deploying frontend

--- Frontend/video-calling-app/src/pages/videoMeet.jsx
+++ Frontend/video-calling-app/src/pages/videoMeet.jsx
@@ -12,11 +12,11 @@ import ScreenShareIcon from "@mui/icons-material/ScreenShare"
 import StopScreenShareIcon from "@mui/icons-material/StopScreenShare"
 import ChatIcon from "@mui/icons-material/Chat"
 import SpeakerNotesOffIcon from '@mui/icons-material/SpeakerNotesOff';
+import server from "../environment";
 
 
 
-
-const server_url = "http://localhost:8000";
+const server_url = server;
 
 var connections ={};
 
