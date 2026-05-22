4ca97973d55f99f42de7e0b2a0f99865ea266e8d
Aakriti <bomzanaakriti@gmail.com>
2026-05-22 11:23:29 +0545

    Improving for deployment

--- Frontend/video-calling-app/src/pages/videoMeet.jsx
+++ Frontend/video-calling-app/src/pages/videoMeet.jsx
@@ -12,7 +12,7 @@ import ScreenShareIcon from "@mui/icons-material/ScreenShare"
 import StopScreenShareIcon from "@mui/icons-material/StopScreenShare"
 import ChatIcon from "@mui/icons-material/Chat"
 import SpeakerNotesOffIcon from '@mui/icons-material/SpeakerNotesOff';
-import server from "../environment";
+import server from "../environment.js";
 
 
 
